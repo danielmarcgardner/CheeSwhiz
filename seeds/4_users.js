7482f3eb5aca946c290c1a1107339845308afca2
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-03-18 18:50:30 -0700

    users and favorites seeds tidied

--- seeds/4_users.js
+++ seeds/4_users.js
@@ -8,27 +8,27 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({
           id: 1,
           email: 'reidpierredelahunt@gmail.com',
-          hashed_password: '',
+          hashed_password: '$2a$10$MRUMlhoWF9v7OwC53j.x3OL/R7FNmxbjHO30ZxOpPeHx.esnpSxtO', //cheese1
           super: true
         }),
         knex('users').insert({
           id: 2,
           email: 'daniel.marc.gardner@gmail.com',
-          hashed_password: '',
+          hashed_password: '$2a$10$ndDF1KKZ49JMiDPn5c9xI.rqICqIm72l4bMxLQ4xTZmpk9qM0YCTq', //cheese2
           super: true
-        })//,
-        /*knex('users').insert({
+        }),
+        knex('users').insert({
           id: 3,
-          email: '',
-          hashed_password: '',
+          email: 'kdawg@yahoo.com',
+          hashed_password: '$2a$10$CtAplCADL7eJPYKwomK6huS5/d48VDbEW2xaiITltch6cAZiHqzsi', //cheese3
           super: false
         }),
         knex('users').insert({
           id: 4, true
-          email: '',
-          hashed_password: '',
+          email: 'klam@earthlink.net',
+          hashed_password: '$2a$10$jT3FoTu2zmqydmtrk06j6.UVaFMHOQ3VfrEsv9/ESTv4uJch.PShy', //cheese4
           super: false
-        })*/
+        })
       ]);
     }).then(function() {
       return knex.raw(`SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites))`);
--- seeds/5_favorites.js
+++ seeds/5_favorites.js
@@ -24,7 +24,27 @@ exports.seed = function(knex, Promise) {
           id: 4,
           user_id: 2,
           cheese_id: 51
+        }),
+        knex('favorites').insert({
+          id: 5,
+          user_id: 3,
+          cheese_id: 47
+        }),
+        knex('favorites').insert({
+          id: 6,
+          user_id: 3,
+          cheese_id: 21
+        }),
+        knex('favorites').insert({
+          id: 7,
+          user_id: 4,
+          cheese_id: 54
         })
+        knex('favorites').insert({
+          id: 8,
+          user_id: 4,
+          cheese_id: 36
+        }),
       ]);
     }).then(function() {
       return knex.raw(`SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites))`);
