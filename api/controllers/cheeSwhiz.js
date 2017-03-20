0dc1fb41a9764782643bda22c862dcaab10d83d9
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-03-19 17:54:53 -0700

    fixed patch route

--- api/controllers/cheeSwhiz.js
+++ api/controllers/cheeSwhiz.js
@@ -76,28 +76,20 @@ function postCheese(req, res, next) {
   });
 }
 
-// SWAGGER CANNOT FIND THIS AND ITS NOT A SPELLING ERROR!!!!!!!!!!!
-function updatedCheese(req, res) {
-  console.log('hello')
+function updatedCheese(req, res, next) {
   const knex = require('../../knex.js')
-  const id = Number(req.params.id);
-  const updatedVersion = {
-    id: id,
-    name: req.body.name,
-    animal_id: Number(req.body.animal_id),
-    firmness_id: Number(req.body.firmness_id),
-    user_id: Number(req.body.user_id)
-  }
-  // knex('cheeses').where('cheeses.id', id)
-  // .then((oneOrNone) => {
-  //   if (oneOrNone.length === 0) {
-  //     res.set('Content-Type', 'plain');
-  //     res.status(400).send('Cheese not found!');
-  //   }
-  // }).then(() => {
+  const id = Number.parseInt(req.swagger.params.id.value);
+
+  knex('cheeses').max('id')
+  .then((maxNum) => {
+    if (maxNum[0].max < id || id < 0) {
+      console.log("I am an invalid request!!")
+      //Swagger will not let me send error code. I cannot do a next() or anything!!!!!!!!!!!!
+    }
+  })
+  const updatedVersion = req.body
   knex('cheeses').where('cheeses.id', id).update(updatedVersion, '*')
   .then((cheese) => {
-    console.log(cheese)
     res.status(200).json(cheese);
   }).catch((err) => {
     console.error(err);
--- test/api/controllers/random.test.js
+++ test/api/controllers/random.test.js
@@ -6,122 +6,122 @@ const expect = require('chai').expect;
 process.env.NODE_ENV = 'test';
 
 beforeEach((done) => {
-  Promise.all([
-    knex('animals').insert({
-      id:1,
-      name: 'cow'
-    }),
-    knex('animals').insert({
-      id:2,
-      name: 'goat'
-    }),
-    knex('animals').insert({
-      id:3,
-      name: 'sheep'
-    }),
-    knex('animals').insert({
-      id:4,
-      name: 'buffalo'
-    }),
-    knex('firmness').insert({
-      id:1,
-      name: 'hard'
-    }),
-    knex('firmness').insert({
-      id:2,
-      name: 'semi-hard'
-    }),
-    knex('firmness').insert({
-      id:3,
-      name: 'semi-soft'
-    }),
-    knex('firmness').insert({
-      id:4,
-      name: 'soft'
-    }),
-    knex('users').insert({
-      id:1,
-      email: 'reidpierredelahunt@gmail.com',
-      hashed_password: '$2a$10$MRUMlhoWF9v7OwC53j.x3OL/R7FNmxbjHO30ZxOpPeHx.esnpSxtO',
-      super: true
-    }),
-    knex('users').insert({
-      id:2,
-      email: 'daniel.marc.gardner@gmail.com',
-      hashed_password: '$2a$10$ndDF1KKZ49JMiDPn5c9xI.rqICqIm72l4bMxLQ4xTZmpk9qM0YCTq',
-      super: true
-    }),
-    knex('cheeses').insert({
-      id:1,
-      name: 'Manchego',
-      animal_id: 3,
-      firmness_id: 1,
-      user_id: 1
-    }),
-    knex('cheeses').insert({
-      id: 2,
-      name: 'Cheddar',
-      animal_id: 1,
-      firmness_id: 2,
-      user_id: 1
-    }),
-    knex('cheeses').insert({
-      id: 3,
-      name: 'Chevre Bucheron',
-      animal_id: 2,
-      firmness_id: 4,
-      user_id: 1
-    }),
-    knex('cheeses').insert({
-      id: 4,
-      name: 'Buffalo Blue',
-      animal_id: 4,
-      firmness_id: 3,
-      user_id: 1
-    }),
-    knex('favorites').insert({
-      id: 1,
-      user_id: 1,
-      cheese_id: 1,
-      notes: null
-    }),
-    knex('favorite').insert({
-      id:2,
-      user_id: 1,
-      cheese_id: 3,
-      notes: null
-    }),
-    knex('favorites').insert({
-      id:3,
-      user_id: 2,
-      cheese_id: 2,
-      notes: null
-    }),
-    knex('favorites').insert({
-      id:1,
-      user_id: 2,
-      cheese_id: 4,
-      notes: null
+  knex.migrate.latest()
+  .then(() =>{
+  return Promise.all([
+    knex('animals').insert({ id: 1, animal: 'cow' }),
+    knex('animals').insert({ id: 2, animal: 'goat' }),
+    knex('animals').insert({ id: 3, animal: 'sheep' }),
+    knex('animals').insert({ id: 4, animal: 'buffalo' })
+  ])
+  })
+    .then(function() {
+      return knex.raw(`SELECT setval('animals_id_seq', (SELECT MAX(id) FROM animals))`)
     })
-  ]).then(() => done());
+    .then(function() {
+      return Promise.all([
+        knex('firmness').insert({ id: 1, firmness: 'hard' }),
+        knex('firmness').insert({ id: 2, firmness: 'semi-hard' }),
+        knex('firmness').insert({ id: 3, firmness: 'semi-soft' }),
+        knex('firmness').insert({ id: 4, firmness: 'soft' })
+      ])
+    })
+    .then(function() {
+      return knex.raw(`SELECT setval('firmness_id_seq', (SELECT MAX(id) FROM firmness))`)
+    })
+    .then(function(){
+      return Promise.all([
+        knex('users').insert({
+          id: 1,
+          email: 'reidpierredelahunt@gmail.com',
+          hashed_password: '$2a$10$MRUMlhoWF9v7OwC53j.x3OL/R7FNmxbjHO30ZxOpPeHx.esnpSxtO', //cheese1
+          super: true
+        }),
+        knex('users').insert({
+          id: 2,
+          email: 'daniel.marc.gardner@gmail.com',
+          hashed_password: '$2a$10$ndDF1KKZ49JMiDPn5c9xI.rqICqIm72l4bMxLQ4xTZmpk9qM0YCTq', //cheese2
+          super: true
+        })
+      ])
+    })
+    .then(function() {
+      return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))`)
+    })
+  .then(() => {
+    return Promise.all([
+      knex('cheeses').insert({
+        id: 1,
+        name: 'Manchego',
+        animal_id: 3,
+        firmness_id: 1,
+        user_id: 1
+      }),
+      knex('cheeses').insert({
+        id: 2,
+        name: 'Cheddar',
+        animal_id: 1,
+        firmness_id: 2,
+        user_id: 1
+      }),
+      knex('cheeses').insert({
+        id: 3,
+        name: 'Chevre Bucheron',
+        animal_id: 2,
+        firmness_id: 4,
+        user_id: 1
+      }),
+      knex('cheeses').insert({
+        id: 4,
+        name: 'Buffalo Blue',
+        animal_id: 4,
+        firmness_id: 3,
+        user_id: 1
+      })
+    ])
+  })
+  .then(function() {
+    return knex.raw(`SELECT setval('cheeses_id_seq', (SELECT MAX(id) FROM cheeses))`)
+  })
+  .then(() => {
+    return Promise.all([
+      knex('favorites').insert({
+        id: 1,
+        user_id: 1,
+        cheese_id: 1
+      }),
+      knex('favorites').insert({
+        id: 2,
+        user_id: 1,
+        cheese_id: 2
+      }),
+      knex('favorites').insert({
+        id: 3,
+        user_id: 2,
+        cheese_id: 3
+      }),
+      knex('favorites').insert({
+        id: 4,
+        user_id: 2,
+        cheese_id: 4
+      })
+    ])
+  })
+  .then(function() {
+    return knex.raw(`SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites))`)
+  })
+  .then(() => done());
 });
 
 afterEach((done) => {
-  knex('favorites').del()
+  knex.migrate.rollback()
   .then(() => {
-    knex('cheeses').del()
+    done()
   })
-  .then(() => {
-    knex('users').del()
+  .catch((err) => {
+    done(err)
   })
-  .then(() => {
-    knex('firmness').del()
-  })
-  .then(() => {
-    knex('animals').del()
-  })
-  .then(() => done())
-});
+})
 
 describe('CheeSwhiz /cheese/random/{number}/{someParameter} route', (done) => {
 
