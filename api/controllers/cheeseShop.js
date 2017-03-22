b7fbd6e817195ce5f74386eeea8f77d57a6a9582
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-03-22 08:31:08 -0700

    working on yelp validation

--- api/controllers/cheeseShop.js
+++ api/controllers/cheeseShop.js
@@ -13,12 +13,16 @@ function getMeters(i) {
   return Math.round(i*1609.344);
 }
 
-
+//Distance Function isn't returning
 function cheeseShop(req, res) {
   const zip = Number(req.swagger.params.zip.value),
         radiusLimit = getMeters(Number(req.swagger.params.distance.value));
 
-        yelp.search({ term: 'Gourmet Cheese', location: zip, category_filter: 'cheese', distance: radiusLimit, limit: 15 })
+        console.log(req.swagger.params.distance.value)
+        console.log(radiusLimit)
+
+        // Need to figure out distance with Reid in the AM...
+        yelp.search({ term: 'Gourmet Cheese', location: zip, category_filter: 'cheese', limit: 15 })
         .then(function (data) {
           for (var i = 0; i < data.businesses.length; i++) {
             delete data.businesses[i].display_phone;
@@ -39,7 +43,6 @@ function cheeseShop(req, res) {
             delete data.businesses[i].menu_provider;
             delete data.businesses[i].categories;
           }
-
           res.json(data.businesses);
         })
         .catch(function (err) {
@@ -55,24 +58,6 @@ function cheeseShop(req, res) {
   //   else if (Number.isNaN(radiusLimit)) { res.status(400).send('Your distance figure must be an integer!')}
   // }
 
-
-// const yelpAuth = {
-//  oauth_consumer_key: process.env.OAUTH_CONSUMER_KEY,
-//  oauth_consumer_secret: process.env.OAUTH_CONSUMER_SECRET,
-//  oauth_token: process.env.OAUTH_TOKEN,
-//  oauth_token_secret: process.env.OAUTH_TOKEN_SECRET,
-//  serviceProvider: {
-//    //Yelp API Documentation
-//    signatureMethod: 'HMAC-SHA1'
-//  }
-// };
-
-  // const request = require('request');
-  // request(`https://api.yelp.com/v2/search/?term=Gourmet%20Cheese&location=${zip}&radius_filter=${radiusLimit}&category_filter=cheese,grocery?oauth_consumer_key=hezWHxItRyCg0AZaaWHxdg&oauth_consumer_secret=CzsRQ0dRRlGeAWtfQXhggGrHzsc&oauth_token=DxQRMKc5HZcV_1yAe3EGMdSw286XQF-w&oauth_token_secret=jZJwurfXpcVWWDeQUibXpcIMY18`, function(error, response, body) {
-  //   console.log('error:', error); // Print the error if one occurred
-  //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //   console.log('body:', body); // Print the HTML for the Google homepage.
-  // })
 }
 
 module.exports = {
